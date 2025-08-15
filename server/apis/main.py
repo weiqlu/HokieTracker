76f7f52fd69384c80d72eac556f7e247b8ca50da
weiqlu <weiqlu@vt.edu>
2025-08-15 14:27:58 -0400

    added svc and signup endpoint

--- server/apis/main.py
+++ server/apis/main.py
@@ -1,25 +1,24 @@
 from fastapi import FastAPI
-from models.course_tracker_model import Section, User
+import os
+import sys
+
+# Add parent directory to path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+from repo.course_tracker_repo import CourseTrackerRepo
+from service.course_tracker_svc import CourseTrackerSvc
 
 app = FastAPI()
 
+repo = CourseTrackerRepo()
+svc = CourseTrackerSvc(repo=repo)
 
 #####################################################################
 # Routes 
 #####################################################################
 
-@app.post("/user")
-async def create_user(user: User): 
-    return 
-
-@app.post("/section")
-async def track_section(section: Section): 
-    return 
-
-@app.get("/user/{email}/courses")
-async def get_user_sections(email: str):
-    return 
-
-@app.delete("/section/{section_id}")
-async def delete_section(section_id: int):
-    return 
+@app.post("/signup")
+async def create_user(email: str, password: str):
+    user = svc.signup_user(email=email, password=password)
+    return user
--- server/service/course_tracker_svc.py
+++ server/service/course_tracker_svc.py
@@ -1,7 +1,17 @@
 import os
 import sys
 
-class CourseTrackerSvc: 
+# Add parent directory to path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
 
-    def __init__(self):
-        self.courses = []
+from repo.course_tracker_repo import CourseTrackerRepo
+
+class CourseTrackerSvc: 
+    def __init__(self, repo: CourseTrackerRepo):
+        self.repo = repo
+
+    # create a new user
+    def signup_user(self, email: str, password: str): 
+        user = self.repo.create_user(email=email, password=password)
+        return user
